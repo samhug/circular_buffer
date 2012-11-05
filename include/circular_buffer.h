00e321447327a9737c8673c807e34491d2e1bda0
Samuel Hug <samuel.b.hug@gmail.com>
2012-11-04 23:37:53 -0800

    Fixed invalid size bug and imporved test cases

--- include/circular_buffer.h
+++ include/circular_buffer.h
@@ -16,7 +16,6 @@ class CircularBuffer
 
         // Return number of bytes written.
         size_t write(const T *data, size_t count);
-        //size_t write(const T *data, size_t count, size_t overlap);
 
         T* at(size_t offset);
 
@@ -45,47 +44,41 @@ CircularBuffer<T>::~CircularBuffer()
     delete [] data_;
 }
 
-/*template <class T>
-size_t CircularBuffer<T>::write(const T *data, size_t count)
-{
-    return write(data, count, 0);
-}*/
-
 template <class T>
 size_t CircularBuffer<T>::write(const T *data, size_t count)
 {
+    assert(count <= capacity() - size());
+
     if (count == 0) return 0;
 
-    size_t capacity = capacity_;
-    size_t bytes_to_write = std::min(count, capacity - size_)*sizeof(T);
+    size_t items_to_write = std::min(count, capacity() - size());
+    size_t bytes_to_write = items_to_write*sizeof(T);
 
     // Write in a single step
-    if (bytes_to_write <= capacity*sizeof(T) - end_index_)
+    if (bytes_to_write <= capacity()*sizeof(T) - end_index_)
     {
         memcpy((void*)data_ + end_index_, data, bytes_to_write);
         end_index_ += bytes_to_write;
-        if (end_index_ == capacity*sizeof(T)) end_index_ = 0;
+        if (end_index_ == capacity()*sizeof(T)) end_index_ = 0;
     }
     // Write in two steps
     else
     {
-        size_t size_1 = capacity*sizeof(T) - end_index_;
+        size_t size_1 = capacity()*sizeof(T) - end_index_;
         memcpy((void*)data_ + end_index_, data, size_1);
         size_t size_2 = bytes_to_write - size_1;
         memcpy(data_, (void*)data + size_1, size_2);
         end_index_ = size_2;
     }
 
-    size_ += bytes_to_write;
-    return bytes_to_write;
+    size_ += items_to_write;
+    return items_to_write;
 }
 
 template <class T>
 T* CircularBuffer<T>::at(size_t offset)
 {
-    if (offset > size()) {
-        // TODO: Freak out... Idiot Alert.
-    }
+    assert(offset <= size());
 
     return (T*)((void*)data_ + end_index_) - offset;
 }
@@ -99,24 +92,26 @@ size_t CircularBuffer<T>::read(T *data, size_t count)
 template <class T>
 size_t CircularBuffer<T>::read(T *data, size_t count, bool del)
 {
+    assert(count <= size());
+
     if (count == 0) return 0;
 
-    size_t capacity = capacity_;
-    size_t bytes_to_read = std::min(count, size_)*sizeof(T);
+    size_t items_to_read = std::min(count, size());
+    size_t bytes_to_read = items_to_read*sizeof(T);
 
     // Read in a single step
-    if (bytes_to_read <= capacity*sizeof(T) - beg_index_)
+    if (bytes_to_read <= capacity()*sizeof(T) - beg_index_)
     {
         memcpy(data, (void*)data_ + beg_index_, bytes_to_read);
         if (del) {
             beg_index_ += bytes_to_read;
-            if (beg_index_ == capacity*sizeof(T)) beg_index_ = 0;
+            if (beg_index_ == capacity()*sizeof(T)) beg_index_ = 0;
         }
     }
     // Read in two steps
     else
     {
-        size_t size_1 = capacity*sizeof(T) - beg_index_;
+        size_t size_1 = capacity()*sizeof(T) - beg_index_;
         memcpy(data, (void*)data_ + beg_index_, size_1);
         size_t size_2 = bytes_to_read - size_1;
         memcpy((void*)data + size_1, data_, size_2);
@@ -126,9 +121,9 @@ size_t CircularBuffer<T>::read(T *data, size_t count, bool del)
     }
 
     if (del) {
-        size_ -= bytes_to_read;
+        size_ -= items_to_read;
     }
-    return bytes_to_read;
+    return items_to_read;
 }
 
 #endif
